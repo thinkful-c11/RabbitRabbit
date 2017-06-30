5e822a2e14269e7f77befeb39702e4993dd202d0
tckastanek <tckastanek@gmail.com>
2017-06-30 11:19:05 -0400

    rendering teh cheeses

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CheeseList from './components/cheese-list';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <CheeseList cheeses={[
+    'Bath Blue',
+    'Barkham Blue',
+    'Buxton Blue'
+  ]} />,
   document.getElementById('root')
 );
